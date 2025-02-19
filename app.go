e50f88d7086703512f1e6aaec575aafb2bf0b422
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2025-02-19 20:30:16 +0500

    feat: add minimsl routinh

---
+++ app.go
@@ -0,0 +1,44 @@
+package sgshttp
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type route struct {
+	method string
+	path string
+	handler func(w http.ResponseWriter, req *http.Request)
+}
+
+type App struct {
+	Routes []route
+	Port int
+}
+
+func CreateApp() App {
+	return App{}
+}
+
+func (app *App) initApp() {
+
+}
+
+func (app *App) addRoute(method string, path string, handler func(w http.ResponseWriter, req *http.Request)) {
+	app.Routes = append(app.Routes, route{method, path, handler})
+}
+
+func (app *App) Get(path string, handler func(w http.ResponseWriter, req *http.Request)) {
+	app.addRoute("GET", path, handler)
+}
+
+func (app *App) Post(path string, handler func(w http.ResponseWriter, req *http.Request)) {
+	app.addRoute("POST", path, handler)
+}
+
+func (app *App) Listen(port int) {
+	for _, route := range app.Routes {
+		http.HandleFunc(route.path, route.handler)
+	}
+	http.ListenAndServe(fmt.Sprintf(":%d", app.Port), nil)
+}
---
+++ example/hello-world/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	sgs "github.com/Reltig/sgs-http"
+	"net/http"
+)
+
+func main() {
+	app := sgs.CreateApp()
+
+	app.Get("/hello", hello)
+
+	app.Listen(8000)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello World"))
+}
